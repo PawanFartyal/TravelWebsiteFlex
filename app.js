5d8e618c3eaf9f05d5ad8ecd6afa47850a9b9bcf
PawanFartyal <pawan.fartyal@techsuperiors.com>
2024-03-13 10:32:01 +0530

    minor changes

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const sidebar =document.querySelector('.sidebar');
 const close =document.querySelector('.close')
 toggleBtn.addEventListener("click",()=>{
     sidebar.classList.add('showSidebar');
-    document.body.style.overflow="hidden";
+    document.body.style.overflowY="hidden";
 })
 close.addEventListener("click",()=>{
     sidebar.classList.remove('showSidebar');
-    document.body.style.overflow="visible";
+    document.body.style.overflowY="visible";
 })
 window.addEventListener("click",(e)=>{
     if(!e.target.closest(".sidebar") && !e.target.closest(".toggle")){
         sidebar.classList.remove('showSidebar');
-        document.body.style.overflow="visible";
+        document.body.style.overflowY="visible";
     }
 
 })
